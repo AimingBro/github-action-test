bcc67a86d06ffb32764230997b557ce4b39ca51a
Junhyeong Jo <118jjh@naver.com>
2020-11-04 20:30:22 +0900

    123

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const publicPath = './public'
+app.use(express.static(publicPath))
+
+app.listen(port, err => {
+  if (err) throw err
+  console.log('Connected to server http://localhost:3000/')
+})
+
+module.exports = app
